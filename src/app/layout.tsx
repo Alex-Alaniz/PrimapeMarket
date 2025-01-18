f4ab77e1ad511b3ac2d1bc96770091152771d464
devAlex.ᴱᵀᴴ <88956822+Alex-Alaniz@users.noreply.github.com>
2025-01-18 11:40:29 +0000

    Restored to '5f1c56304fa21aa92fd4c105b80480d8fa48e61e'
    Replit-Restored-To:5f1c56304fa21aa92fd4c105b80480d8fa48e61e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ThirdwebProvider>
             {children}
           </ThirdwebProvider>
